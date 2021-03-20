86ccf3be7ffbabb3f02430477cbeea6d70090709
TheGoldenEye <goldeneye@jn11.xyz>
2021-03-20 11:21:20 +0100

    Better error handling

--- index.js
+++ index.js
@@ -31,7 +31,17 @@ axios.get(cfg.url)
 
   })
   .catch(error => {
-    console.log(error);
+    if (error.response) {
+      // The request was made and the server responded with a status code that falls out of the range of 2xx
+      console.error('Status:', error.response.statusText, '(' + error.response.status + ')');
+    } else if (error.request) {
+      // The request was made but no response was received, `error.request` is an instance of XMLHttpRequest in the browser 
+      // and an instance of http.ClientRequest in Node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request and triggered an Error
+      console.log('Error:\n', error);
+    }
   });
 
 
